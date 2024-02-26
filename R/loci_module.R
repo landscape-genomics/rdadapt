465f36849a77de3704aab4624b2f2746c4423817
Thibaut Capblancq <tcapblancq@eduroam-083144.grenet.fr>
2024-02-26 10:52:59 +0100

    Changes on the vignette and work on the loci_module function

---
+++ R/loci_module.R
@@ -0,0 +1,54 @@
+###################################################################################################
+##' @name loci_module
+##' @author Thibaut Capblancq
+##' 
+##' @title Loci clustering
+##' 
+##' @description This function allows the user to find groups of covarying loci on the RDA space.
+##' 
+##' @param RDA blabla
+##' @param K blabla
+##' 
+##' @return  
+##' 
+##' A \code{data.frame} containing :
+##' \itemize{
+##'   \item \code{p.values} : blabla
+##'   \item \code{q.values} : blabla
+##' }
+##' 
+##' 
+##' @details
+##' 
+##' Blablabla
+##' 
+##' 
+##' @keywords 
+##' 
+##' @seealso 
+##' 
+##' @examples
+##' 
+##' 
+##' @importFrom stats kmeans
+##' @importFrom plyr ddply
+##' 
+##' @export
+##' 
+##'
+###################################################################################################
+
+loci_module <- function(RDA, K)
+{
+  # K-means clustering with K = 2
+  clusters <- kmeans(abs(RDA_outliers$CCA$v)[, 1:2], 2, iter.max = 10, nstart = 3)
+  
+  # Convex hulls.
+  df <- data.frame(x = abs(RDA_outliers$CCA$v[, 1])
+                   , y = abs(RDA_outliers$CCA$v[, 2])
+                   , cluster = clusters$cluster)
+  find_hull <- function(df) df[chull(df$x, df$y), ]
+  hulls <- ddply(df, "cluster", find_hull)
+  
+  return(hulls)
+}
