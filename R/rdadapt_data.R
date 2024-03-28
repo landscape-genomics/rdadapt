6cf1e6bf8ea5abfae05c131b69e7267789839a58
MayaGueguen <maya.gueguen@gmail.com>
2024-03-28 12:25:56 +0100

    Move raw data from data/ to data-raw/

--- R/rdadapt_data.R
+++ R/rdadapt_data.R
@@ -10,23 +10,30 @@
 #'   \item{coord}{a \code{data.frame} containing coordinates for 2036 individuals}
 #'   \item{Y}{a \code{data.frame} containing genomic information for 2036 
 #'   individuals about 2333 genes}
+#'   \item{xx.current}{a \code{data.frame} containing 4 current environmental 
+#' variables for 2036 individuals}
+#'   \item{xx.future}{a \code{data.frame} containing 4 future environmental 
+#' variables for 2036 individuals}
+#'   \item{causal1}{a \code{vector} identifying ...}
+#'   \item{causal2}{a \code{vector} identifying ...}
+#'   \item{fitness}{a \code{data.frame} containing ...}
 #' }
 
 "maladapt"
 
 # ## Get population labels
-# pop <- read.csv2("data/pop.csv", sep = ",", head = TRUE)[,-1]
+# pop <- read.csv2("data-raw/pop.csv", sep = ",", head = TRUE)[,-1]
 # 
 # ## Get individual coordinates
-# coord <- read.csv2("data/position.csv", sep = ",", head = TRUE)[,-1]
+# coord <- read.csv2("data-raw/position.csv", sep = ",", head = TRUE)[,-1]
 # coord <- data.frame(apply(coord, 2, as.numeric))
 # 
 # ## Get genetic dataset
-# Y <- read.csv2("data/genome.csv", sep = ",", head = TRUE, row.names = 1)
+# Y <- read.csv2("data-raw/genome.csv", sep = ",", head = TRUE, row.names = 1)
 # 
 # ## Get the current and future climatic variables
-# xx.current <- read.table("data/var_current.csv", sep = ",", head = TRUE, row.names = 1)
-# xx.future <- read.table("data/var_futur.csv", sep = ",", head = TRUE, row.names = 1)
+# xx.current <- read.table("data-raw/var_current.csv", sep = ",", header = TRUE, row.names = 1)
+# xx.future <- read.table("data-raw/var_futur.csv", sep = ",", header = TRUE, row.names = 1)
 # 
 # ## Add extra variables (correlated)
 # VAR3.current <- 2 * xx.current[, 1] - xx.current[, 2] + rnorm(nrow(xx.current), sd = 0.07)
@@ -39,11 +46,11 @@
 # xx.future <- data.frame(xx.future, VAR3 = VAR3.future, VAR4 = VAR4.future)
 # 
 # ## Get adaptive loci for env variable 1 and env variable 2
-# causal1 <- as.numeric(read.csv2("data/mutationm2.csv", sep = ",", header = TRUE)[,-1])
-# causal2 <- as.numeric(read.csv2("data/mutationm3.csv", sep = ",", header = TRUE)[,-1])
+# causal1 <- as.numeric(read.csv2("data-raw/mutationm2.csv", sep = ",", header = TRUE)[,-1])
+# causal2 <- as.numeric(read.csv2("data-raw/mutationm3.csv", sep = ",", header = TRUE)[,-1])
 # 
 # ## Verifying that prediction are matching with a decline in fitness on the landscape
-# fitness <- read.table("data/fitness_futur.csv", sep = ",", header = TRUE)
+# fitness <- read.table("data-raw/fitness_futur.csv", sep = ",", header = TRUE)
 # 
 # 
 # ## Gather verything in a list
